38f456447cdb7319f6f1f692554d16dbf801b916
jaiacedera <jairahacedera@gmail.com>
2026-02-09 18:15:40 +0800

    commit 1

---
+++ FlappyBirdTrial/Assets/Scripts/PipeMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PipeMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed = 5;
+    public float deadZone = -1;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if (transform.position.x < deadZone )
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
+    }
+}
